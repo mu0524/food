2f8ee663e3b78a96028480f67498b32728f862e7
mu0524 <aays2025@gmail.com>
2021-04-18 01:01:48 +0800

    20210418 嵌入js

--- program/js/map.js
+++ program/js/map.js
@@ -17,3 +17,23 @@ readTextFile("D:/git_workplace/food/program/pig.json", function(text){
 });
 
 //win+r chrome.exe --allow-file-access-from-files
+
+//地圖
+let map;
+document.addEventListener("DOMContentLoaded", () => {
+let s = document.createElement("script");
+        document.head.appendChild(s);
+        s.addEventListener("load", () => {
+            //script has loaded
+            console.log("script has loaded");
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: {
+                    lat:23.695919378044756, 
+                    lng: 120.53353474084176
+                },
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+        });
+        s.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDj79YdxxvDWfJ6ji3IEO31iTPrL8555vI&callback=initMap`; //請確定金鑰正確(AIzaSyDj79YdxxvDWfJ6ji3IEO31iTPrL8555vI)
+});
