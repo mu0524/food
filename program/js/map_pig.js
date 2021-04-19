0f0ede1d8368689771607299ac66f4f7c3861a80
shang-yu621 <sherry.31010.123@gmail.com>
2021-04-19 14:42:43 +0800

    0419暫存檔

--- program/js/map_pig.js
+++ program/js/map_pig.js
@@ -15,5 +15,21 @@ readTextFile("D:/git_workplace/food/program/pig.json", function(text){
     var data = JSON.parse(text);
     console.log(data);
 });
+/*
+var map;
+function initMap() {
+    var position = {
+      lat: 23.695919378044756,
+      lng: 120.53353474084176
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: position
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+  }*/
 
 //win+r chrome.exe --allow-file-access-from-files
