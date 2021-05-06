657d5df2d01aa317b4f4bafe5e3bee5283287871
mu0524 <aays2025@gmail.com>
2021-05-06 18:16:57 +0800

    20210506 地圖標記+套bt框架

--- program/js/map.js
+++ program/js/map.js
@@ -18,22 +18,80 @@ readTextFile("D:/git_workplace/food/program/pig.json", function(text){
 
 //win+r chrome.exe --allow-file-access-from-files
 
-//地圖
-let map;
-document.addEventListener("DOMContentLoaded", () => {
-let s = document.createElement("script");
-        document.head.appendChild(s);
-        s.addEventListener("load", () => {
-            //script has loaded
-            console.log("script has loaded");
-            map = new google.maps.Map(document.getElementById("map"), {
-                center: {
-                    lat:23.695919378044756, 
-                    lng: 120.53353474084176
-                },
-                zoom: 16,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
+
+$(function() {
+
+    var map = document.getElementById("map");
+          
+    var latlng = new google.maps.LatLng(23.695919378044756, 120.53353474084176);
+          
+    var mapOptions = {
+        zoom: 16,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false
+    };
+          
+    var mapObj = new google.maps.Map(map, mapOptions);
+    var marker = [];
+    var infoWindow = [];
+          
+    // 地圖標記
+    var markerData = [
+            {
+                position: new google.maps.LatLng(23.6904173, 120.5289109),
+                title: '測試',
+                content: '我家對面啦',
+                icon: 'https://i.imgur.com/PKdLaxG.png',
+            },
+            {
+                position: new google.maps.LatLng(23.693625, 120.5274303),
+                title: '萊爾富',
+                content: 'ㄚ就萊爾富',
+                icon: 'https://i.imgur.com/PKdLaxG.png',
+            }
+    ];
+          
+            // 標記在地圖上
+            for (i = 0; i< markerData.length; i++) {
+              marker[i] = new google.maps.Marker({
+                position: markerData[i].position,
+                map: mapObj,
+                title: markerData[i].title,
+                icon: markerData[i].icon,
+              });
+          
+              infoWindow[i] = new google.maps.InfoWindow({ // 訊息框
+                content: '<div class="name">' + markerData[i]['title']+'<br>'+ markerData[i]['content'] + '</div>'
+              });
+              
+              markerEvent(i);
+            }
+            
+            // 標記訊息框
+            function markerEvent(i) {
+              marker[i].addListener('click', function() {
+                infoWindow[i].open(mapObj, marker[i]);
+              });
+            }
+            
+            // 餐廳搜尋 - 他有點問題但我暫時找不到
+            $('.form-inline').submit(function() {
+              setAddress($('.form-control').val());
+              return false;
             });
-        });
-        s.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDj79YdxxvDWfJ6ji3IEO31iTPrL8555vI&callback=initMap`; //請確定金鑰正確(AIzaSyDj79YdxxvDWfJ6ji3IEO31iTPrL8555vI)
+          
+            
+            function setAddress(address) {
+                var sad = address;
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({'address':sad}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        mapObj.setCenter(results[0].geometry.location);
+                    } else {
+                        alert('找不到');
+                    }
+                });
+            }
+          
 });
