44245e6ec818014bdea7bb2a49fd8d469452d926
mu0524 <aays2025@gmail.com>
2021-08-10 18:13:53 +0800

    0810 註冊ok 登入0 安全0

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-from flask import Flask, render_template, url_for,redirect, request, jsonify
+from flask import Flask, render_template, url_for,redirect, request, jsonify, flash
+from pymongo import message
 import dbController as db
-import json
+import os
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16).hex()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.login_message = '請先登入'
+
+
+class User(UserMixin):
+    pass
+
+#確認使用者是否是在我們的合法清單users當中，若沒有，就什麼都不做。
+#若有，就宣告一個我們剛才用UserMixin做出來的物件User()，貼上user標籤，
+#並回傳給呼叫這個函數user_loader()的地方
+@login_manager.user_loader
+def user_loader(email): 
+    users = db.selectUser(email)
+    if email not in users:
+        return
+    user = User()
+    user.id = email
+    return user
 
 @app.route('/')
 def index():
@@ -28,13 +53,38 @@ def user():
 @app.route('/login', methods=['POST','GET']) #有表單(form)就需要methods這一個
 def login():
     if request.method == 'POST': #請求method是post的資料
-        if request.values['login-btn'] == 'login': #html頁面login按鈕 按鈕name的value等於login
-            return redirect(url_for('index')) #跳轉至index function(前往index頁面)
+        email = request.form['email_input']
+        pwd = request.form['pwd_input']
+        data = db.selectUserInfo(email)
+        if pwd == data[2]:
+            user = User()
+            user.id = email
+            uid = data[0]
+            login_user(user)
+            return redirect(url_for('login'),uid)
+        else:
+            return redirect(url_for('login'),alert='帳號密碼輸入錯誤')
+        
+        
+        # if request.values['login-btn'] == 'login': #html頁面login按鈕 按鈕name的value等於login
+        #     return redirect(url_for('index')) #跳轉至index function(前往index頁面)
     else:
         return render_template('login.html') 
 
-@app.route('/signin')
+@app.route('/signin', methods=["GET", "POST"])
 def signin():
+    if request.method == 'POST':
+        email = request.form['email_input']
+        users = db.selectUser(email)
+        if len(users) == 0:
+            userID = request.form['uid_input']
+            pwd = request.form['pwd_input']
+            if(db.insertUser(userID,email,pwd)):
+                flash('註冊成功，請登入')
+                return redirect(url_for('login'))
+        else:
+            flash('此email已註冊過，請重新輸入')
+            return redirect(url_for('signin'))
     return render_template('signin.html') 
 
 @app.route('/about')
--- dbController.py
+++ dbController.py
@@ -1,5 +1,8 @@
+# from food.app import user
 import  pymongo
 import ssl
+
+from pymongo import results
 
 #資料庫連線
 client = pymongo.MongoClient("mongodb+srv://admin:admin@mytopic.hpirm.mongodb.net/mytopicDB", ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
@@ -8,11 +11,32 @@ collpig = db.twpig #確認資料集
 colleva = db.evaluation
 collfre = db.free
 collgre = db.green
+colluser = db.User
 
 #coll.stats #確認是否連線
 
 
+
 def selectPigdata():
-    result = list(collpig.find())   #抓資料
+    result = collpig.find()  #抓資料
     result = [ {**item, **{"_id": str(item["_id"])} } for item in result ] #處理 objectId 轉string
     return result
+
+#找全部使用者
+def selectUser(email):
+    result = colluser.find({'email': email})  #抓資料
+    result = [ {**item, **{"_id": str(item["_id"])} } for item in result ] #處理 objectId 轉string
+    return result
+
+#找使用者個人資訊
+def selectUserInfo(email):
+    result = colluser.find_one({'email': email})
+    userID = result['userID']
+    email = result['email']
+    password = result['pwd']
+    return userID,email,password
+
+#新增使用者
+def insertUser(userID,email,pwd):
+    colluser.insert_one({'userID': userID, 'email':email, 'pwd':pwd})
+    return True
