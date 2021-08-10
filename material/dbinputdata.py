535f5b5b00a05ec258cf2a3951df4de03ef66dec
shang-yu621 <sherry.31010.123@gmail.com>
2021-08-10 23:51:57 +0800

    Revert "0810資料庫更新"
    This reverts commit 190246aa840500f98f8ab3da779cb095938c3a5c.

--- material/dbinputdata.py
+++ material/dbinputdata.py
@@ -4,45 +4,23 @@ import json
 
 client = pymongo.MongoClient("mongodb+srv://admin:admin@mytopic.hpirm.mongodb.net/mytopicDB")
 db = client.mytopicDB
-#coll = db.twpig #確認資料集
-coll = db.free
+coll = db.twpig #確認資料集
+#coll = db.evaluation
+#coll = db.free
 #coll = db.green
 
-
-#測試用資料庫
-# client = pymongo.MongoClient("mongodb+srv://admin:admin@mytopic.hpirm.mongodb.net/mytopicDB")
-# db = client.mytryQAQ
-# coll = db.pigpig #確認資料集
 
 coll.stats #確認是否連線
 
 
-with open('D:\\git_workplace\\food\\material\\data_free.json', newline='',encoding = 'utf8') as jsonfile: #注意路徑
+with open('D:\\git_workplace\\food\\program\\data_pig.json', newline='',encoding = 'utf8') as jsonfile: #注意路徑
     data = json.load(jsonfile)
-    
 
-"""
-#台灣豬適用，去除所有換行
-a=0
-for elem in data:
-    elem["market_name"]=elem["market_name"].replace("\n","")
-    elem["context"]=elem["context"].replace("\n","")
-    elem["addr"]=elem["addr"].replace("\n","")
-    a+=1
-    if a%80==0:
-        print("*",end="")"""
-
+print(data)
 
 if isinstance(data, list):
-    coll.insert_many(data)
+    coll.insert_many(data)  
 else:
     coll.insert_one(data)
 
-print("OK~")
 
-"""
-#寫資料庫的find可以參考
-result = list(collpig.find({"market_name": {"$regex":"快車肉乾"}}))
-for element in result:
-    print(element)
-    print("\n\n\n\n\n")"""
