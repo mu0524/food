eb3f29bbe128a63c74494283bc35ad14b7e844b7
shang-yu621 <sherry.31010.123@gmail.com>
2021-08-19 00:50:24 +0800

    0819 資料庫統一

--- dbController.py
+++ dbController.py
@@ -16,18 +16,18 @@ collmat = db.material
 #coll.stats #確認是否連線
 
 def selectPigdata():
-    result = collpig.find({},{"_id":0,"market_name":1,"addr":1,"Latitude": 1,"Lontitude": 1,"context":1})  #抓資料
+    result = collpig.find({},{"_id":0,"market_name":1,"addr":1,"Latitude": 1,"Lontitude": 1,"context":1,"Tel":1})  #抓資料
     #result = [ {**item, **{"_id": str(item["_id"])} } for item in result ] #處理 objectId 轉string
     result = [ {**item } for item in result ] #處理 objectId 轉string
     return result
 
 def selectGreenData():
-    result = collgre.find({},{"_id":0,"Name":1,"Address":1,"Response_X": 1,"Response_Y": 1,"Tel":1})  #抓資料
+    result = collgre.find({},{"_id":0,"market_name":1,"addr":1,"Lontitude": 1,"Latitude": 1,"Tel":1,"context":1})  #抓資料
     result = [ {**item } for item in result ] #處理 objectId 轉string
     return result
 
 def selectFreeData():
-    result = collfre.find({},{"_id":0,"SupplierName":1,"Address":1,"Response_X": 1,"Response_Y": 1,"GBType":1,"Tel":1})  #抓資料
+    result = collfre.find({},{"_id":0,"market_name":1,"addr":1,"Lontitude": 1,"Latitude": 1,"context":1,"Tel":1})  #抓資料
     result = [ {**item } for item in result ] #處理 objectId 轉string
     return result
 
